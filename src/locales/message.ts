c909270cd022f9807a50673975ee845a85ef344f
Julien Calixte <juliencalixte@gmail.com>
2020-09-19 14:09:40 +0200

    :bug: (i18n) fix i18n configuration

---
+++ src/locales/message.ts
@@ -0,0 +1,9 @@
+import en from './en.json'
+import fr from './fr.json'
+
+export const messages = {
+  en,
+  fr
+}
+
+export const locales = Object.keys(messages)
--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,13 @@ import '@/registerServiceWorker'
 import App from './App.vue'
 import { createApp } from 'vue'
 import { createI18n } from 'vue-i18n'
-import { en } from '@/locales/en'
+import { messages } from '@/locales/message'
 import { router } from '@/router'
 import { store } from '@/store'
 
 const i18n = createI18n({
   locale: 'en',
-  messages: {
-    en
-  }
+  messages
 })
 
 createApp(App)
