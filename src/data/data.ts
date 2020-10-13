d4875c75d5684e3437d98c91e0769591695f170e
Julien Calixte <julienc@bam.tech>
2020-10-13 22:29:55 +0200

    :sparkles: (data) add crud methods

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,24 +1,61 @@
 import PouchDb from 'pouchdb-browser'
 import { Model } from './models/Model'
 
-class Data {
-  #locale = new PouchDb()
+interface GetAllParams {
+  prefix?: string
+  includeDocs?: boolean
+  includeAttachments?: boolean
+}
 
-  public async add(model: Model) {
+class Data {
+  private locale = new PouchDb()
+
+  public async add(model: Model): Promise<boolean> {
     try {
-      const result = await this.#locale.put(model)
+      const result = await this.locale.put(model)
       return result.ok
-    } catch (error) {
+    } catch {
       return false
     }
   }
 
-  public async get(id: string) {
+  public async remove(id: string): Promise<boolean> {
     try {
-      return await this.#locale.get(id) || null
-    } catch (error) {
+      const doc = await this.get(id)
+      if (!doc) {
+        return false
+      }
+      const result = await this.locale.put({
+        ...doc,
+        _deleted: true
+      })
+      return result.ok
+    } catch {
+      return false
+    }
+  }
+
+  public async get<T extends Model>(id: string): Promise<T | null> {
+    try {
+      return ((await this.locale.get(id)) as T) || null
+    } catch {
       return null
     }
+  }
+
+  public async getAll<T extends Model>({
+    prefix,
+    includeDocs = true,
+    includeAttachments = false
+  }: GetAllParams): Promise<T[]> {
+    const response = await this.locale.allDocs({
+      include_docs: includeDocs,
+      attachments: includeAttachments,
+      startkey: prefix ? prefix : undefined,
+      endkey: prefix ? `${prefix}\ufff0` : undefined
+    })
+
+    return response.rows.map((row) => row.doc) as T[]
   }
 }
 
